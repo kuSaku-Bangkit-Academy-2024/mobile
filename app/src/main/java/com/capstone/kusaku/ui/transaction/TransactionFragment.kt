b9ee335da61cc246c45ab5368e7f432202f18473
fadhlanhasyim <fadhlanhasyim54@gmail.com>
2024-06-02 01:45:05 +0700

    feat: transaction page

---
+++ app/src/main/java/com/capstone/kusaku/ui/transaction/TransactionFragment.kt
@@ -0,0 +1,29 @@
+package com.capstone.kusaku.ui.transaction
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.capstone.kusaku.databinding.FragmentTransactionBinding
+
+class TransactionFragment : Fragment() {
+    private var _binding: FragmentTransactionBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentTransactionBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
