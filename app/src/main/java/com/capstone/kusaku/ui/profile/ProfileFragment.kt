026f47891f8cedeb3773155b1a67792dbd07d7ac
fadhlanhasyim <fadhlanhasyim54@gmail.com>
2024-06-02 01:44:52 +0700

    feat: profile page

---
+++ app/src/main/java/com/capstone/kusaku/ui/profile/ProfileFragment.kt
@@ -0,0 +1,46 @@
+package com.capstone.kusaku.ui.profile
+
+import android.content.Intent
+import androidx.fragment.app.viewModels
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import com.capstone.kusaku.databinding.FragmentProfileBinding
+import com.capstone.kusaku.ui.login.LoginActivity
+
+class ProfileFragment : Fragment() {
+    private var _binding: FragmentProfileBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.valueUsername.text = "Poli Deo Lolowang"
+        binding.valueEmail.text = "poli.deo@gmail.com"
+        binding.valueIncome.text = "Rp.50.000.000"
+        binding.valueGender.text = "Male"
+
+        binding.btnLogout.setOnClickListener {
+            Toast.makeText(requireContext(), "Logged out", Toast.LENGTH_SHORT).show()
+            val intent = Intent(activity, LoginActivity::class.java)
+            startActivity(intent)
+
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/capstone/kusaku/ui/profile/ProfileViewModel.kt
@@ -0,0 +1,7 @@
+package com.capstone.kusaku.ui.profile
+
+import androidx.lifecycle.ViewModel
+
+class ProfileViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
