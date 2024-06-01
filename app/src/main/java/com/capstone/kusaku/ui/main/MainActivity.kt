f6552a3d12e46962ce9ac334ce3b17b4ae366ea9
fadhlanhasyim <fadhlanhasyim54@gmail.com>
2024-06-02 01:44:17 +0700

    feat: registration page

--- app/src/main/java/com/capstone/kusaku/ui/main/MainActivity.kt
+++ app/src/main/java/com/capstone/kusaku/ui/main/MainActivity.kt
@@ -4,8 +4,6 @@ import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.capstone.kusaku.R
 import com.capstone.kusaku.databinding.ActivityMainBinding
@@ -23,14 +21,6 @@ class MainActivity : AppCompatActivity() {
         val navView: BottomNavigationView = binding.navView
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
 }
--- app/src/main/java/com/capstone/kusaku/ui/register/RegisterActivity.kt
+++ app/src/main/java/com/capstone/kusaku/ui/register/RegisterActivity.kt
@@ -1,21 +1,59 @@
 package com.capstone.kusaku.ui.register
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
+import android.text.style.ForegroundColorSpan
+import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.content.ContextCompat
 import com.capstone.kusaku.R
+import com.capstone.kusaku.databinding.ActivityRegisterBinding
+import com.capstone.kusaku.ui.login.LoginActivity
+import com.capstone.kusaku.ui.main.MainActivity
 
 class RegisterActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRegisterBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_register)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupSignInText()
+        binding.btnRegister.setOnClickListener {
+            val intent = Intent(this@RegisterActivity, MainActivity::class.java)
+            startActivity(intent)
         }
+    }
+
+    private fun setupSignInText() {
+        val tvSignIn: TextView = binding.tvToSignIn
+        val text = getString(R.string.got_an_account)
+
+        val spannableString = SpannableString(text)
+
+        val clickableSpan = object : ClickableSpan() {
+            override fun onClick(widget: View) {
+                val intent = Intent(this@RegisterActivity, LoginActivity::class.java)
+                startActivity(intent)
+            }
+        }
+
+        val greenColor = ContextCompat.getColor(this, R.color.teal_700)
+        val greenColorSpan = ForegroundColorSpan(greenColor)
+
+        val startIndex = text.indexOf("Sign In")
+        val endIndex = startIndex + "Sign In".length
+
+        spannableString.setSpan(clickableSpan, startIndex, endIndex, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+        spannableString.setSpan(greenColorSpan, startIndex, endIndex, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+        tvSignIn.text = spannableString
+        tvSignIn.movementMethod = LinkMovementMethod.getInstance()
     }
 }
