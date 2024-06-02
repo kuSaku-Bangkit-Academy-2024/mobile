b2eec1615714a1424d0cb4d4ed729202a8a7a9fe
Fadhlan Hasyim <fadhlanhasyim54@gmail.com>
2024-06-02 11:35:17 +0700

    Feat: registration, profile, and transaction page (#2)
    * feat: registration page
    
    * feat: profile page
    
    * feat: transaction page
    
    * feat: registration asset
    
    * fix: remove unused imports
    
    * fix: wrong copy writing

---
+++ app/src/main/java/com/capstone/kusaku/ui/transaction/TransactionFragment.kt
@@ -0,0 +1,27 @@
+package com.capstone.kusaku.ui.transaction
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.capstone.kusaku.databinding.FragmentTransactionBinding
+
+class TransactionFragment : Fragment() {
+    private var _binding: FragmentTransactionBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentTransactionBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
